5a7deba18669302601f45fa0a75f1cb5d067149c
Edson Cruz <jhunnior.93@gmail.com>
2023-06-07 22:41:56 -0400

    rename unicate to courseinfo

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercise}
+  </p>
+);
+
+const Content = (props) => {
+  const [part1, part2, part3] = props.parts;
+  return (
+    <div>
+      <Part part={part1.name} exercise={part1.exercises} />
+      <Part part={part2.name} exercise={part2.exercises} />
+      <Part part={part3.name} exercise={part3.exercises} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const exercises = props.parts.map((part) => part.exercises)
+  let total = 0;
+
+  exercises.forEach((exercise) => {
+    total += exercise;
+  });
+
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
