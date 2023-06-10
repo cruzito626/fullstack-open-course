c01d7698f7d15fe4b4c9373487e45e698a1877e6
Edson Cruz <jhunnior.93@gmail.com>
2023-06-09 22:27:00 -0400

    course info step six

---
+++ part1/courseinfo/src/components/Course.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./Header";
+import Content from "./Content";
+
+const Course = ({ course }) => {
+  const { name, parts } = course;
+  return (
+    <>
+      <Header title={name} />
+      <Content parts={parts} />
+    </>
+  );
+};
+
+
+export default Course;
